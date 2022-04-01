221ac6fd3516d5c244820c2f01599b1e4f7aa175
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-04-01 23:40:56 +0600

    updated

--- src/Layout/Base/Base.js
+++ src/Layout/Base/Base.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+
+import './Base.css';
 function Base(props) {
     return (
         <div id='Base'>
+            <div className="Base_design_wrapper">
+                <div className="design_left">
+                    <img src={require('../../Static/img/3d_illustration.png')} alt="" />
+                </div>
+                <div className="design_head">
+                    <img src={require('../../Static/img/head.png')} alt="img" />
+                </div>
+            </div>
             <Header></Header>
             {props.children}
             <Footer></Footer>
