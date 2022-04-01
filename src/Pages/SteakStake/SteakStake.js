4c62911027672594b469b12d97f547404b58c0d1
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-04-01 16:07:58 +0600

    section2 done

--- src/Pages/SteakStake/SteakStake.js
+++ src/Pages/SteakStake/SteakStake.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./SteakStake.css";
+import CalculatorsSvg from "../../Components/Icons/SvgIcons/CalculatorsSvg";
 function SteakStake(props) {
   return (
     <div id="SteakStake">
+      <Section1></Section1>
+      <Section2></Section2>
+    </div>
+  );
+}
+
+export default SteakStake;
+
+const Section1 = () => {
+  return (
+    <section id="Section1">
       <div className="section_container">
         <div className="section_title">
           <h2 className="title">Farms</h2>
@@ -83,7 +95,8 @@ function SteakStake(props) {
                 </li>
 
                 <li>
-                  <img style={{height: '2.5rem', marginLeft: '1.2rem'}}
+                  <img
+                    style={{ height: "2.5rem", marginLeft: "1.2rem" }}
                     src={require("../../Static/img/meat_box_frame.png")}
                     alt=""
                   />
@@ -101,8 +114,65 @@ function SteakStake(props) {
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
-}
+};
 
-export default SteakStake;
+const Section2 = () => {
+  return (
+    <section id="Section2">
+      <div className="section_container">
+        <div className="section_body">
+          <div className="section_box_wrapper">
+            <div className="section_body_box_1">
+              <img src={require("../../Static/img/img_b.png")} alt="" />
+              <ul className="section_content_list">
+                <li>VIP Pool</li>
+                <li>Stake BITX to earn BUSD and User Tier</li>
+                <li>
+                  APR 33.08% <CalculatorsSvg />
+                </li>
+                <li>
+                  Total Staked
+                  <strong>4,445,998.00 BITX</strong>
+                </li>
+              </ul>
+            </div>
+            <div className="section_body_box_2">
+              <div className="section_box_2_content">
+                <ul className="section_content_list">
+                  <li>
+                    Your current tier <span>No Tier</span>
+                  </li>
+                  <li>Token will be locked 30 days</li>
+
+                  <li>
+                    <span>Earned</span>
+                    <strong>0.000</strong>
+                  </li>
+                  <li className="dropdown_btn_list_wrapper">
+                    <button className="dropdown_btn">
+                      Detail{" "}
+                      <svg
+                        width={14}
+                        height={14}
+                        viewBox="0 0 14 14"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M7 8.75L7.41248 9.16248L7 9.57496L6.58752 9.16248L7 8.75ZM10.9125 5.66248L7.41248 9.16248L6.58752 8.33752L10.0875 4.83752L10.9125 5.66248ZM6.58752 9.16248L3.08752 5.66248L3.91248 4.83752L7.41248 8.33752L6.58752 9.16248Z"
+                          fill="#FAB674"
+                        />
+                      </svg>
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
