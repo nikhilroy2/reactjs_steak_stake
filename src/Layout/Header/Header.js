6807e2276f529ae7d8bc508806ca55788ea67bee
Nikhil Roy <nikhilchandraroypoet@gmail.com>
2022-04-02 09:07:16 +0600

    header aside responsive done

--- src/Layout/Header/Header.js
+++ src/Layout/Header/Header.js
@@ -33,6 +33,27 @@ function Header(props) {
       classList: "",
     },
   ];
+
+  const nav_list_user = [
+    {
+      id: 1,
+      amount: "$0.0055",
+      action: "#",
+      icon: require("../../Static/img/icons/icon1.png"),
+    },
+    {
+      id: 2,
+      amount: "$0.0055",
+      action: "#",
+      icon: require("../../Static/img/icons/icon2.png"),
+    },
+    {
+      id: 3,
+      amount: "$0.0004",
+      action: "#",
+      icon: require("../../Static/img/icons/icon3.png"),
+    },
+  ];
   const [scrollClass, setScrollClass] = useState("");
   window.addEventListener("scroll", (e) => {
     if (
@@ -44,10 +65,62 @@ function Header(props) {
       setScrollClass("");
     }
   });
+
+  const [navToggle, setNavToggle] = useState(false);
+
   return (
-    <header id="Header" className={scrollClass}>
-      <div className="section_container">
+    <header
+      id="Header"
+      className={`${scrollClass} ${navToggle ? "toggleActiveHeader" : ""}`}
+    >
+      <div className="header_container">
         <nav id="header_nav">
+          <button
+            className="nav_toggle_btn"
+            onClick={() => setNavToggle(!navToggle)}
+          >
+            {navToggle ? (
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                strokeWidth={0}
+                viewBox="0 0 512 512"
+                className="hidden mr-6 text-4xl cursor-pointer lg:block"
+                height="2.3rem"
+                width="2.3rem"
+                xmlns="http://www.w3.org/2000/svg"
+                style={{ zIndex: 99 }}
+              >
+                <path
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={32}
+                  d="M368 368L144 144m224 0L144 368"
+                />
+              </svg>
+            ) : (
+              <svg
+                stroke="currentColor"
+                fill="#fff"
+                strokeWidth={0}
+                viewBox="0 0 512 512"
+                className="hidden mr-6 text-4xl cursor-pointer lg:block"
+                height="2.3rem"
+                width="2.3rem"
+                xmlns="http://www.w3.org/2000/svg"
+                style={{ zIndex: 99 }}
+              >
+                <path
+                  fill="#fff"
+                  strokeLinecap="round"
+                  strokeMiterlimit={10}
+                  strokeWidth={48}
+                  d="M88 152h336M88 256h336M88 360h336"
+                />
+              </svg>
+            )}
+          </button>
           <a href="#" className="logo_link">
             <img src={require("../../Static/img/logo.png")} alt="logo" />
           </a>
@@ -64,33 +137,15 @@ function Header(props) {
           </ul>
 
           <ul className="nav_list_user">
-            <li>
-              <button className="nav_light_btn">
-                <img
-                  src={require("../../Static/img/icons/icon1.png")}
-                  alt="icon"
-                />{" "}
-                $0.0055
-              </button>
-            </li>
-            <li>
-              <button className="nav_light_btn">
-                <img
-                  src={require("../../Static/img/icons/icon2.png")}
-                  alt="icon"
-                />{" "}
-                $0.0055
-              </button>
-            </li>
-            <li>
-              <button className="nav_light_btn">
-                <img
-                  src={require("../../Static/img/icons/icon3.png")}
-                  alt="icon"
-                />{" "}
-                $0.0004
-              </button>
-            </li>
+            {nav_list_user.map((v) => {
+              return (
+                <li key={v.id}>
+                  <button className="nav_light_btn">
+                    <img src={v.icon} alt="icon" /> {v.amount}
+                  </button>
+                </li>
+              );
+            })}
 
             <li>
               <button className="connect_btn">Connect Wallet</button>
@@ -98,8 +153,48 @@ function Header(props) {
           </ul>
         </nav>
       </div>
+      <HeaderAside
+        navToggle={navToggle}
+        nav_list_menu={nav_list_menu}
+        nav_list_user={nav_list_user}
+      ></HeaderAside>
     </header>
   );
 }
 
 export default Header;
+
+const HeaderAside = ({ navToggle, nav_list_menu, nav_list_user }) => {
+  return (
+    <aside id="HeaderAside" className={navToggle && "HeaderAsideActive"}>
+      <div className={`aside_box_wrapper ${navToggle && "aside_active"}`}>
+        <ul className="nav_list_menu">
+          {nav_list_menu.map((v) => {
+            return (
+              <li key={v.id}>
+                <a href={v.link} className={v.classList}>
+                  {v.name}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="aside_nav_content">
+          <ul className="nav_list_user_box">
+            {nav_list_user.map((v) => {
+              return (
+                <li key={v.id}>
+                  <button className="nav_light_btn">
+                    <img src={v.icon} alt="icon" /> {v.amount}
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+          <strong>Total Value Locked</strong>
+          <strong>$2,649.86</strong>
+        </div>
+      </div>
+    </aside>
+  );
+};
